194ea63d0ac0b8ac661f5505c0480f2ee70c8f2c
Richard Boothe <richardjboothe@me.com>
2016-11-01 23:48:18 -0500

    create main.js and add to index.html

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 "use strict";
-let express = require('express');
+let express    = require('express');
 let bodyParser = require('body-parser');
-let morgan = require('morgan')
-let app = express();
-let router = express.Router();
-let API = require('./routes/APIRouter');
+let morgan     = require('morgan')
+let app        = express();
+let router     = express.Router();
+let API        = require('./routes/APIRouter');
 
-//middleware
+//MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('dev',{
@@ -15,14 +15,15 @@ app.use(morgan('dev',{
 }));
 
 // SERVE UP THOSE DELICIOUS STATIC FILES!
-app.use( express.static(__dirname + '/../public') );
+app.use( express.static(__dirname + '/../client') );
 
-app.get('/', function(req,res) {
+app.get('/', (req,res) =>   {
   res.send('Hello Yoda!')
 });
 
 //ROUTERS
 app.use("/api", API);
 
+//ESTABLISH SERVER AND START SERVER
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), () => { console.log(`"To Do" app is running on port: ${app.get('port')}`)});
