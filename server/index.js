37105987123690af6db3ee4c06b484c49758ebe0
Richard Boothe <richardjboothe@me.com>
2016-11-01 22:57:25 -0500

    server running

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 "use strict";
+let express = require('express');
+let bodyparser = require('body-parser');
+let morgan = require('morgan')
+let app = express();
+let router = express.Router();
+
+app.get('/', function(req,res) {
+  res.send('Hello Yoda!')
+});
+
+
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), () => { console.log(`"To Do" app is running on port: ${app.get('port')}`)});
