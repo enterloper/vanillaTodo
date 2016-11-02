8ecd16458998c44cb2cadcfe3d294e62b66a3fed
richardjboothe <richardjboothe@gmail.com>
2016-11-02 10:56:06 -0500

    created user schema for mongoose

--- server/index.js
+++ server/index.js
@@ -8,15 +8,17 @@ let router     = express.Router();
 let API        = require('./routes/APIRouter');
 
 //mongodb connection
-mongoose.connect("mongodb://localhost:4545/notebook");
+mongoose.connect("mongodb://localhost:27018/notebook");
 // variable for the above connection using the connection property on the mongoose object
 let db = mongoose.connection;
 //error handling for mongodb
 db.on('error', console.error.bind(console, 'connection error:'));
 
 //MIDDLEWARE
+//parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+//print out requests
 app.use(morgan('dev',{
   // only log error responses
   // skip: function (req, res) { return res.statusCode < 400; }
